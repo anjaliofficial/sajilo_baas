8d987bdc3b9f7ae453b11f291834cf87bce7e07a
anjaliofficial <bistaanjali1415@gmail.com>
2026-02-22 10:19:57 +0545

    Booking entity

---
+++ lib/features/booking/domain/entities/booking_entity.dart
@@ -0,0 +1,25 @@
+class BookingEntity {
+  final String id;
+  final String listingId;
+  final String hostId;
+  final DateTime checkInDate;
+  final DateTime checkOutDate;
+  final int totalNights;
+  final double pricePerNight;
+  final double totalPrice;
+  final String status;
+  final String paymentStatus;
+
+  BookingEntity({
+    required this.id,
+    required this.listingId,
+    required this.hostId,
+    required this.checkInDate,
+    required this.checkOutDate,
+    required this.totalNights,
+    required this.pricePerNight,
+    required this.totalPrice,
+    required this.status,
+    required this.paymentStatus,
+  });
+}
