176d3728372d177f92867f12a3e011832d450521
anjaliofficial <bistaanjali1415@gmail.com>
2026-02-22 10:24:55 +0545

    booking model

---
+++ lib/features/booking/data/models/booking_model.dart
@@ -0,0 +1,33 @@
+import '../../domain/entities/booking_entity.dart';
+
+class BookingModel extends BookingEntity {
+  BookingModel({
+    required super.id,
+    required super.listingId,
+    required super.hostId,
+    required super.checkInDate,
+    required super.checkOutDate,
+    required super.totalNights,
+    required super.pricePerNight,
+    required super.totalPrice,
+    required super.status,
+    required super.paymentStatus,
+  });
+
+  factory BookingModel.fromJson(Map<String, dynamic> json) {
+    return BookingModel(
+      id: json['_id'],
+      listingId: json['listingId'] is Map
+          ? json['listingId']['_id']
+          : json['listingId'],
+      hostId: json['hostId'] is Map ? json['hostId']['_id'] : json['hostId'],
+      checkInDate: DateTime.parse(json['checkInDate']),
+      checkOutDate: DateTime.parse(json['checkOutDate']),
+      totalNights: json['totalNights'],
+      pricePerNight: (json['pricePerNight'] as num).toDouble(),
+      totalPrice: (json['totalPrice'] as num).toDouble(),
+      status: json['status'],
+      paymentStatus: json['paymentStatus'],
+    );
+  }
+}
